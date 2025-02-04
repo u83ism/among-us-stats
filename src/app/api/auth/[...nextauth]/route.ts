e2d7b1fd863551ad44ff5649d8f8a3c3ec73fa07
ゆうやみ(yu-yami) <u83ism@gmail.com>
2025-02-05 02:28:57 +0900

    💚 デプロイに失敗する問題を修正

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,13 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-const clientId = process.env.GOOGLE_CLIENT_ID;
-const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+// VERCELのデプロイの際にbuildコマンドが走り、その時は環境変数が定義されてない。
+// nullがありうるが型定義上はnullを許容しないので、NULL合体演算子を使って空文字に変換する
+const clientId = process.env.GOOGLE_CLIENT_ID ?? '';
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET ?? '';
 
-console.log(clientId, clientSecret)
-
-if (!clientId || !clientSecret) {
-	throw new Error("Missing GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET environment variables");
-}
+console.log(`clientId: ${clientId}`);
+console.log(`clientSecret: ${clientSecret}`);
 
 const handler = NextAuth({
 	// Google
