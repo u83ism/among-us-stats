7618b1e376ac95ac945506fdf25867f606ed63e7
ゆうやみ(yu-yami) <u83ism@gmail.com>
2025-02-05 02:46:53 +0900

    🔇 ロギングを削除

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,6 @@ import GoogleProvider from "next-auth/providers/google"
 const clientId = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
-console.log(clientId, clientSecret)
-
 // ⚠️ VERCELの管理画面から環境変数を設定しておかないと、デプロイの際のビルドで落ちるので注意
 if (!clientId || !clientSecret) {
 	throw new Error("Missing GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET environment variables");
