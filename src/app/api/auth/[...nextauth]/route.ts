576dd379e9f5a7eb43ee496fcedc67271ffa6407
ゆうやみ(yu-yami) <u83ism@gmail.com>
2025-02-05 02:31:13 +0900

    Revert "💚 デプロイに失敗する問題を修正"
    This reverts commit e2d7b1fd863551ad44ff5649d8f8a3c3ec73fa07.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,14 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-// VERCELのデプロイの際にbuildコマンドが走り、その時は環境変数が定義されてない。
-// nullがありうるが型定義上はnullを許容しないので、NULL合体演算子を使って空文字に変換する
-const clientId = process.env.GOOGLE_CLIENT_ID ?? '';
-const clientSecret = process.env.GOOGLE_CLIENT_SECRET ?? '';
+const clientId = process.env.GOOGLE_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
-console.log(`clientId: ${clientId}`);
-console.log(`clientSecret: ${clientSecret}`);
+console.log(clientId, clientSecret)
+
+if (!clientId || !clientSecret) {
+	throw new Error("Missing GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET environment variables");
+}
 
 const handler = NextAuth({
 	// Google
