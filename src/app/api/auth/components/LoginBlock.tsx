40e3931e9c426982a7d49e27b2c3ad1f7b171a92
ゆうやみ(yu-yami) <u83ism@gmail.com>
2025-02-15 20:45:12 +0900

    💄 ログインページのGUIを修正

--- src/app/api/auth/components/LoginBlock.tsx
+++ src/app/api/auth/components/LoginBlock.tsx
@@ -8,14 +8,19 @@ const LoginBlock = (): React.JSX.Element => {
 		case "loading":
 			return <div>⏳ Loading...</div>;
 		case "authenticated":
-			return <div>✅ あなたはログイン済みです</div>;
+			throw new Error('ログイン済みなのにログインブロックを表示しようとしています')
 		case "unauthenticated":
 			return (
 				<div>
-					<p>🔒 あなたはログインしていません</p>
-					<button onClick={() => signIn("google", {}, { prompt: "login" })}>
-						Googleでログイン
-					</button>
+					<div>🔒 あなたはログインしていません</div>
+					<div>
+						<button
+							className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+							onClick={() => signIn("google", {}, { prompt: "login" })}
+						>
+							ログインボタン
+						</button>
+					</div>
 				</div>
 			);
 		default:
